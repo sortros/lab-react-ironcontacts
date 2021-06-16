dce57c240db153e1ef7b86b0d86410bf1fd326e1
Josep <albiol957@hotmail.com>
2021-06-16 15:58:34 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 import contacts from "./contacts.json";
+import React from 'react';
 
 
 const arr2=contacts.slice(0,5)
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {contactList: arr2};
+  }
+
+  addRandom=() => {
+    console.log(contacts[Math.floor(Math.random() * contacts.length)])
+    const randomContact= contacts[Math.floor(Math.random() * contacts.length)]
+    const contactListCopy= [...this.state.contactList]
+    contactListCopy.push(randomContact)
+    this.setState( {contactList: contactListCopy})
+
+  }
+
+  render() {  
+    return(
     <div className="App">
-      <table>
-        <tr>
-          <th> Picture </th>
-          <th>Name</th>
-          <th>Popularity</th>
-        </tr>
+    <table>
+      <tr>
+        <th> Picture </th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    
+    {this.state.contactList.map(contact => {
+      console.log(arr2);
       
-      {arr2.map(contact => {
-        console.log(arr2);
-        
-          return (
-          <tr>
-            <td> <img width="50px" src={contact.pictureUrl}/> </td>
-            <td>{contact.name}</td>
-            <td>{contact.popularity}</td>
-          </tr>
-        )
-        
-      })}
-      </table>
-    </div>
-  );
+        return (
+        <tr>
+          <td> <img width="50px" src={contact.pictureUrl}/> </td>
+          <td>{contact.name}</td>
+          <td>{contact.popularity}</td>
+        </tr>
+      )
+      
+      
+    })}
+    </table>
+    <button onClick={this.addRandom}>Add Random Contact</button>
+  </div>)
+    
+}
 }
 export default App;
