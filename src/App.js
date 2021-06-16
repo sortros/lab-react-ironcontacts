200b333406d875be2fde59e1c9d47d650df093ca
Josep <albiol957@hotmail.com>
2021-06-16 13:13:44 +0200

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+
+
+const arr2=contacts.slice(0,5)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th> Picture </th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+      
+      {arr2.map(contact => {
+        console.log(arr2);
+        
+          return (
+          <tr>
+            <td> <img width="50px" src={contact.pictureUrl}/> </td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+          </tr>
+        )
+        
+      })}
+      </table>
     </div>
   );
 }
-
 export default App;
